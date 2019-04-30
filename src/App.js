4488c5cf5e725f0971326464de252d8cc686ab37
kanjamad bosel <kanjamadpor@gmail.com>
2019-04-30 10:52:45 -0700

    change h1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Hello World</h1>
+        <h1>Hello World Kanjamad</h1>
       </div>
     );
   }
